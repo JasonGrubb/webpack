23f026ac308c2789c575a004de40e3cdff5ae34e
grubbjt <grubbjt@appstate.edu>
2017-03-08 09:51:31 -0500

    All files updated to point to Component

--- template/src/App.vue
+++ template/src/App.vue
@@ -4,20 +4,20 @@
     {{#router}}
     <router-view></router-view>
     {{else}}
-    <hello></hello>
+    <component></component>
     {{/router}}
   </div>
 </template>
 
 <script>
 {{#unless router}}
-import Hello from './components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Component from './components/Component'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 {{/unless}}
 export default {
   name: 'app'{{#router}}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{else}},
   components: {
-    Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    Component{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
   }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{/router}}
 }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 </script>
