82905d42e4ed1e93bf4fa138cb6f80e67cc6c0da
grubbjt <grubbjt@appstate.edu>
2017-03-08 09:47:40 -0500

    Rolling back to Hello temporarily

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="componentname">
+  <div class="hello">
     <h1>\{{ msg }}</h1>
+    <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -21,7 +22,7 @@
 
 <script>
 export default {
-  name: 'componentname',
+  name: 'hello',
   data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
     return {
       msg: 'Welcome to Your Vue.js App'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
