d8f0c92eddf8eb64881e254a858765bc0582b9cb
Ryan Belt <beltrw@appstate.edu>
2017-04-27 13:49:59 -0400

    Added date filter (#7)
    Added font-awesome
    imported bulma styles

--- template/src/main.js
+++ template/src/main.js
@@ -3,9 +3,17 @@ import App from './App'
 import router from './router'
 import store from './store/store'
 import Buefy from 'buefy'
+import 'bulma/css/bulma.css'
+import 'font-awesome/css/font-awesome.css'
 import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
+
+Vue.filter('formatDate', function (value) {
+  if (value) {
+    return moment(String(value)).format('DD-MMM-YYYY')
+  }
+})
 
 Vue.component(Buefy.Table.name, Buefy.Table)
 
