c20b6fc4cd5b6042e200e3ec6fa8e80d98ab2990
Ryan Belt <beltrw@appstate.edu>
2017-04-27 13:16:23 -0400

    Added bulma and buefy (#6)

--- template/src/main.js
+++ template/src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/store'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
+
+Vue.component(Buefy.Table.name, Buefy.Table)
 
 /* eslint-disable no-new */
 new Vue({
